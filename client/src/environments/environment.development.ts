908ab819ca9995adb33f695ea22b7ec483287cbf
DarkoJohnny13 <darkotadic724@gmail.com>
2025-05-11 06:32:02 +0200

    End of  section 18

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  hubUrl: 'https://localhost:5001/hub/notifications',
   stripePublicKey:
     'pk_test_51Qp3AADl5Jd2IctpWAnBeN9iWGGV5jKXOSFrGLUvxAHfSL4IvbhE9dxBD3ro9TOWc4ihlud6WzetwxgFab01GE2Q0083AKOh7R',
 };
